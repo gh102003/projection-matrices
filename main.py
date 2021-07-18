8adcdb50ce4e5f8a43000701628edb6041b49b67
George Howarth <george@pyesmeadow.com>
2021-07-18 22:34:34 +0100

    Draw using tkinter canvas functions

--- main.py
+++ main.py
@@ -4,41 +4,15 @@ import itertools
 import tkinter as tk
 import time
 
-# radius = 10, centre = (0, 0, 0)
-def point_in_sphere(point):
-    if point.shape != (3,):
-        raise ValueError
-
-    orig_dist = (point ** 2).sum() ** 0.5
-    return orig_dist <= 10
-
-
-# sidelength = 20, centre = (0, 0, 0)
-def point_in_cube(point):
-    if point.shape != (3,):
-        raise ValueError
-
-    # true if all ordinates between -10 and 10
-    return np.all([-10 <= point, point <= 10])
-
-
-# world is a 3 x n matrix, each column representing a possible coord
-axis_values = np.linspace(-10, 10, 80)
-# world = np.array(list(itertools.product(
-    # axis_values, axis_values, axis_values)))
-# world = list(filter(point_in_cube, world))
-# world = np.array(world).T
-world = np.array(np.meshgrid(axis_values, axis_values, axis_values)).T.reshape(-1, 3).T
-
 
 # 2x3 matrix to map 3D to 2D
 base_projection = np.array([[10, 0, 0],
-                            [0, 0, 10]])
+                            [0, 0, 10]], dtype=np.float64)
 
 
 class Camera:
     def __init__(self):
-        self.pos = np.array([0, 0, 0]).reshape((3, 1))
+        self.pos = np.array([0, 0, 0], dtype=np.float64).reshape((3, 1))
         # pitch, roll, and yaw are angles (in radians) of anticlockwise rotations about the x, y and z axes respectively
         self.pitch = 0
         self.roll = 0
@@ -62,96 +36,210 @@ class Camera:
         return rotation
 
     def project(self, world):
-        start = time.perf_counter()
+        """
+        Takes an array of 3d points and projects them into an array of 2d points
+        world - 3xn matrix, each column representing a 3d point
+        returns a 2xn matrix, each column representing a 2d point
+        """
+        # start = time.perf_counter()
         # screen is a 3 x n matrix, each column representing coords of a value to be drawn
-        screen = np.dot(base_projection, np.dot(
-            self.create_rotation_matrix(), world - self.pos))
-        print(
-            f"Projection took {1000 * (time.perf_counter() - start):4.0f} ms")
+        screen = np.dot(base_projection, self.pos -
+                        np.dot(self.create_rotation_matrix(), world))
+        # print(
+        # f"Projection took {1000 * (time.perf_counter() - start):4.0f} ms")
         return screen
 
 
 camera = Camera()
 
+
+class Renderer:
+    def __init__(self, tkinter_canvas):
+        self._canvas: tk.Canvas = tkinter_canvas
+
+    def render_line(self, a, b):
+        """
+            Projects and renders a line onto the canvas
+
+            a, b - arrays (length 3) representing two 3d points
+        """
+        points = np.array([a, b]).T
+        projected = camera.project(points)
+        a2d, b2d = projected.T
+        self._canvas.create_line(a2d[0], a2d[1], b2d[0], b2d[1])
+
+    def render_quad(self, a, b, c, d):
+        """
+            Projects and renders a line onto the canvas
+
+            a, b, c, d - arrays (length 3) representing four 3d points
+        """
+        points = np.array([a, b, c, d]).T
+        projected = camera.project(points)
+        a2d, b2d, c2d, d2d = projected.T
+        self._canvas.create_polygon(
+            *a2d, *b2d, *c2d, *d2d, width=3, outline="black", fill="")
+
+    def render_cube(self, a, b):
+        """
+            Projects and renders a line onto the canvas
+
+            a, b - arrays (length 3) representing two 3d points on opposite corners of the cube
+        """
+        points = np.array([
+            # bottom
+            a,
+            [b[0], a[1], a[2]],
+            [b[0], b[1], a[2]],
+            [a[0], b[1], a[2]],
+            # top
+            [a[0], a[1], b[2]],
+            [b[0], a[1], b[2]],
+            b,
+            [a[0], b[1], b[2]],
+        ]).T
+        projected = camera.project(points)
+        p, q, r, s, t, u, v, w = projected.T
+        # bottom
+        self._canvas.create_line(*p, *q)
+        self._canvas.create_line(*q, *r)
+        self._canvas.create_line(*r, *s)
+        self._canvas.create_line(*s, *p)
+        # top
+        self._canvas.create_line(*t, *u)
+        self._canvas.create_line(*u, *v)
+        self._canvas.create_line(*v, *w)
+        self._canvas.create_line(*w, *t)
+        # vertical
+        self._canvas.create_line(*p, *t)
+        self._canvas.create_line(*q, *u)
+        self._canvas.create_line(*r, *v)
+        self._canvas.create_line(*s, *w)
+
+
 # RENDERING ONTO CANVAS
 
-def render_PIL(canvas):
-    start = time.perf_counter()
+def render_PIL(canvas, renderer):
+    
     canvas.delete("all")
 
-    # draw points
-    # points = camera.project(world).T
-    points = camera.project(world)
-    pixels = np.full((400, 400), 255, dtype=np.uint8)
-    a = time.perf_counter()
-    converted = (points).astype(int, casting='unsafe') + 200 # convert to ints and move origin to screen centre
-
-    # filter out any coords with a value below 0 (would cause index to wrap around)
-    filtered = converted[:, np.all(
-        [converted[0] >= 0, converted[1] >= 0, converted[0] < 400, converted[1] < 400], axis=0)]
-
-    try:
-        pixels[filtered[1], filtered[0]] = 0
-    except IndexError:
-        pass
-
-    print(f"Points -> Pixels took {1000 * (time.perf_counter() - a):4.0f} ms")
-
-    window.image = ImageTk.PhotoImage(Image.fromarray(
-        pixels, "L"))  # store as attribute to prevent GC
-    canvas.create_image(0, 0, image=window.image, anchor=tk.NW)
+    p1 = [2, 2, 5]
+    p2 = [2, 30, 5]
+    p3 = [30, 30, 5]
+    p4 = [30, 2, 5]
+    renderer.render_quad(p1, p2, p3, p4)
+    renderer.render_cube([2, 2, 2], [9, 9, 9])
 
     # draw origin
-    origin = camera.project(np.array([[0, 0, 0]]).T).T[0] + 200
+    origin = camera.project(np.array([[0, 0, 0]]).T).T[0]
     canvas.create_oval(origin[0], origin[1],
                        origin[0] + 8, origin[1] + 8, fill="red")
-
-    print(
-        f"Whole render took {1000 * (time.perf_counter() - start):4.0f} ms\n")
 
 
 def key_press(event):
     global camera
 
     if event.char == "w":
-        camera.pos[2, 0] += -1
-    if event.char == "s":
         camera.pos[2, 0] += 1
+    if event.char == "s":
+        camera.pos[2, 0] += -1
     if event.char == "a":
-        camera.pos[0, 0] += -1
-    if event.char == "d":
         camera.pos[0, 0] += 1
+    if event.char == "d":
+        camera.pos[0, 0] += -1
     if event.char == "q":
-        camera.roll += -0.2
+        camera.roll += -0.05
     if event.char == "e":
-        camera.roll += 0.2
+        camera.roll += 0.05
     if event.char == "r":
-        camera.pitch += -0.2
+        camera.pitch += -0.05
     if event.char == "f":
-        camera.pitch += 0.2
+        camera.pitch += 0.05
 
-    render_PIL(canvas)
+    # render_PIL(canvas, renderer)
+
+render_batch_start_time = None
+render_batch_size = 20
+def frame(n=0):
+    global camera, render_batch_start_time
+    camera.yaw += 0.005
+    
+    # measure performance by measuring frame rate in batches of 20
+    if n % render_batch_size == 0:
+        if render_batch_start_time is not None:
+            batch_duration_ms = 1000 * (time.perf_counter() - render_batch_start_time)
+            print(f"Render x {render_batch_size} took {batch_duration_ms:4.0f} ms ({batch_duration_ms / render_batch_size:2.0f} ms avg, {batch_duration_ms / render_batch_size:2.0f} FPS)")
+        render_batch_start_time = time.perf_counter()
+    render_PIL(canvas, renderer)
 
 
-def spinning_animation():
-    global camera
-    camera.yaw += 0.03
-    render_PIL(canvas)
-    window.after(150, spinning_animation)
+    window.after(30, lambda: frame(n + 1))
+
+
+
+last_b1_x = None
+last_b1_y = None
+
+def b1_drag(event):
+    global camera, last_b1_x, last_b1_y
+
+    if last_b1_x is not None:
+        delta_x = event.x - last_b1_x
+        camera.yaw += delta_x / 100
+
+    if last_b1_y is not None:
+        delta_y = event.y - last_b1_y
+        camera.pitch += delta_y / 100
+
+    last_b1_x = event.x
+    last_b1_y = event.y
+
+def b1_release(event):
+    global last_b1_x, last_b1_y
+    last_b1_x = None
+    last_b1_y = None
+
+
+last_b2_x = None
+last_b2_y = None
+
+def b3_drag(event):
+    global camera, last_b2_x, last_b2_y
+
+    if last_b2_x is not None:
+        delta_x = event.x - last_b2_x
+        camera.pos[0, 0] += delta_x / 10
+
+    if last_b2_y is not None:
+        delta_y = event.y - last_b2_y
+        camera.pos[2, 0] += delta_y / 10
+
+    last_b2_x = event.x
+    last_b2_y = event.y
+
+def b3_release(event):
+    global last_b2_x, last_b2_y
+    last_b2_x = None
+    last_b2_y = None
 
 
 window = tk.Tk()
 window.title("Projection Matrices")
 canvas = tk.Canvas(width=400, height=400, bg="white")
+renderer = Renderer(canvas)
 canvas.pack()
-render_PIL(canvas)
-window.bind("w", key_press)
-window.bind("s", key_press)
-window.bind("a", key_press)
-window.bind("d", key_press)
-window.bind("q", key_press)
-window.bind("e", key_press)
-window.bind("r", key_press)
-window.bind("f", key_press)
-window.after(150, spinning_animation)
+render_PIL(canvas, renderer)
+window.bind("<B1-Motion>", b1_drag)
+window.bind("<ButtonRelease-1>", b1_release)
+window.bind("<B3-Motion>", b3_drag)
+window.bind("<ButtonRelease-3>", b3_release)
+# window.bind("w", key_press)
+# window.bind("s", key_press)
+# window.bind("a", key_press)
+# window.bind("d", key_press)
+# window.bind("q", key_press)
+# window.bind("e", key_press)
+# window.bind("r", key_press)
+# window.bind("f", key_press)
+window.after(30, frame)
 window.mainloop()
